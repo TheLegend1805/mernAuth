aecedfd937450573c5543ba499b0dcfa0113c641
TheLegend1805 <vaibhavacharya46@gmail.com>
2024-08-08 02:19:09 +0530

    connect to db, port no

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-app.listen(4000, () => {
-  console.log(`server on ${3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server on ${process.env.PORT}`);
 });
