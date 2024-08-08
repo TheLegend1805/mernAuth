3c8794ce65dda8f1368965b8b5155df4cd6504a1
TheLegend1805 <vaibhavacharya46@gmail.com>
2024-08-09 00:14:06 +0530

    create signin api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js";
 import bcrytjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,6 +14,40 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All Fields required" });
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not found"));
+    }
+
+    const validPassword = bcrytjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashPassword, ...rest } = validUser._doc;
+    // ._doc property contains the raw data of the document as a plain JavaScript object.
+
+    const cookieExpiry = new Date(Date.now() + 3600000);
+    //add cookie expiry for 1hr. accesstoken will be same for 1hr.
+    res
+      .cookie("accesstoken", token, { httpOnly: true, expires: cookieExpiry })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
